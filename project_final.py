1d43d76c10f45610709aa7a60cdd0217d7cda3de
RyanDancoes <75823998+RyanDancoes@users.noreply.github.com>
2021-04-27 22:26:34 -0400

    Update project_final.py

--- project_final.py
+++ project_final.py
@@ -1,3 +1,13 @@
+"""
+Name: Ryan Dancoes
+CS230: SN1
+Data: skyscrapers.csv
+URL: https://share.streamlit.io/ryandancoes/exploring_skyscrapers/main/project_final.py
+
+Description: Three informative graphs exploring the history and location of skyscrapers built
+around the world using data adapted from Wikipedia.
+"""
+
 import pandas as pd
 import streamlit as st
 import pydeck as pdk
@@ -5,41 +15,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def summer(num, check):
+    if num == 1 and check:
+        return True
+    return False
+
+
 def home():
     st.title('Welcome to Ryan\'s final project')
+    number = np.random.randint(0, 5)
+    mood = st.checkbox('')
+    check = summer(number, mood)
+    if check:
+        st.write('Have a great summer break!')
 
 
 def heatmap(dataframe):
+
     st.title('Heatmap')
     year = st.slider('Show skyscrapers constructed up to:', 1930, 2020)
-    st.write('Concentration of skyscrapers in green -> blue')
-    df_map = dataframe[dataframe['Year'] <= year]
-    blue_scale = [[240, 249, 232], [204, 235, 197], [168, 221, 181],
-                  [123, 204, 196], [67, 162, 202], [8, 104, 172]]
+    st.write('Concentration of skyscrapers (red > yellow):')
 
+    # creates new dataframe with dates before selected year
+    df_map = dataframe[dataframe['Year'] <= year]
+
+    # building the deck
     view_state = pdk.ViewState(
         latitude=0,
         longitude=0,
-        zoom=0.3
-    )
+        zoom=0.3)
     layer = pdk.Layer(
+        # heatmap of skyscraper density in an area
         'HeatmapLayer',
         data=df_map,
         get_position='[Lon, Lat]',
-        radius=1000,
-        pickable=True,
-        opacity=0.95,
-        color_range=blue_scale,
-        threshold=0.25
-    )
+        radius=1000)
+
+    # display map
     st.pydeck_chart(pdk.Deck(
-        map_style='mapbox://styles/mapbox/dark-v9',
+        map_style='mapbox://styles/mapbox/light-v9',
         initial_view_state=view_state,
-        layers=layer
-    ))
+        layers=layer))
 
 
 def multiplot(dataframe):
+    
+    # initializing and setting variables/dictionaries
+    l_sky, l_comm, l_conc, l_co_st, l_steel, l_chim, l_latt = \
+        [], [], [], [], [], [], []
+    x_sky, x_comm, x_conc, x_co_st, x_steel, x_chim, x_latt = \
+        [], [], [], [], [], [], []
+    types = {'sky': 'Skyscraper', 'comm': 'Communications tower',
+             'conc': 'Concrete tower', 'co_st': 'Concrete/steel tower',
+             'steel': 'Steel tower', 'chim': 'Chimney', 'latt': 'Lattice tower'}
+    
     st.title('Multiplot')
     st.write('Select which types of skyscraper to display:')
     sky = st.checkbox('Skyscraper')
@@ -50,27 +79,28 @@ def multiplot(dataframe):
     chim = st.checkbox('Chimney')
     latt = st.checkbox('Lattice Tower')
     yticks = np.arange(0, 50, 5)
-    fig, ax = plt.subplots()
+    
+    # Sorted existing df and set index back to [0, 1, 2,...]
     df_sorted = dataframe.sort_values('Year').reset_index()
-    l_sky, l_comm, l_conc, l_co_st, l_steel, l_chim, l_latt = \
-        [], [], [], [], [], [], []
-    x_sky, x_comm, x_conc, x_co_st, x_steel, x_chim, x_latt = \
-        [], [], [], [], [], [], []
-    types = {'sky': 'Skyscraper', 'comm': 'Communications tower',
-             'conc': 'Concrete tower', 'co_st': 'Concrete/steel tower',
-             'steel': 'Steel tower', 'chim': 'Chimney', 'latt': 'Lattice tower'}
+    
+    # link shortname with full type name
     for name, type_ in types.items():
         year, n, l, x = 0, 0, [], []
+        
+        # iterate through rows of building type
         for building in df_sorted[df_sorted['Type'] == type_].itertuples():
             year = building.Year
             x.append(year)
             n += 1
             l.append(n)
+        
+        # if no skyscrapers built in 2020, continued the line all the way across
         if year < 2020:
             for i in range(2020-year):
                 year += 1
                 x.append(year)
                 l.append(n)
+        
         if name == 'sky':
             x_sky = x
             l_sky = l
@@ -92,7 +122,9 @@ def multiplot(dataframe):
         elif name == 'latt':
             x_latt = x
             l_latt = l
-    if sky:
+            
+    fig, ax = plt.subplots()
+    if sky:  # if checkbox checked
         ax.step(x_sky, l_sky, where='post')
     if comm:
         ax.step(x_comm, l_comm, where='post')
@@ -109,15 +141,15 @@ def multiplot(dataframe):
     ax.set_title('Multiplot Graph')
     ax.set_xlabel('Year')
     ax.set_ylabel('Skyscrapers built')
-    ax.set_xbound(1929, 2021)
+    ax.set_xbound(1929, 2021)  # static x and y-axis
     ax.set_ybound(0, 50)
     ax.set_yticks(yticks)
     st.pyplot(fig)
 
 
 def bar(dataframe):
-    st.title('Bar')
-    year = st.slider('Show skyscrapers constructed up to:', 1930, 2020)
+    
+    # initialize and set variables/dictionaries
     locations = {'Africa': [], 'Antarctica': [],
                  'Asia': ['United Arab Emirates', 'Japan', 'China',
                           'Saudi Arabia', 'South Korea', 'Russia', 'Taiwan',
@@ -129,7 +161,14 @@ def bar(dataframe):
                  'South America': [], 'Oceania': []}
     f_africa, f_antarctic, f_asia, f_europe, f_north, f_south, f_ocean = \
         0, 0, 0, 0, 0, 0, 0
+    
+    st.title('Bar')
+    year = st.slider('Show skyscrapers constructed up to:', 1930, 2020)
+    
+    # new df like before (before date selected)
     df_bar = dataframe[dataframe['Year'] <= year]
+    
+    # I decided to do this the long way (could have just used df.count())
     for country in df_bar.Country:
         if country[1:] in locations['Africa']:
             f_africa += 1
@@ -145,15 +184,17 @@ def bar(dataframe):
             f_south += 1
         elif country[1:] in locations['Oceania']:
             f_ocean += 1
+    
+    # graphing
     fig, ax = plt.subplots()
     x = [cont for cont in locations.keys()]
     freq = [f_africa, f_antarctic, f_asia, f_europe, f_north, f_south, f_ocean]
     for i in range(len(x)):
         ax.bar(x[i], freq[i])
-        ax.set_title('Bar Chart through time')
-        ax.set_ylabel('Skyscrapers built')
-        ax.set_xticklabels(x, rotation=45)
-        ax.set_ybound(0, 53)
+    ax.set_title('Bar Chart through time')
+    ax.set_ylabel('Skyscrapers built')
+    ax.set_xticklabels(x, rotation=45)
+    ax.set_ybound(0, 53)
     st.pyplot(fig)
 
 
@@ -162,9 +203,8 @@ with open('skyscrapers.csv', 'r') as file:
 
 display = st.sidebar.selectbox(
     'Which chart would you like to display?',
-    ('None', '3D Map', 'Multiplot', 'Bar')
-)
-if display == 'None':
+    ('', '3D Map', 'Multiplot', 'Bar'))
+if display == '':
     home()
 elif display == '3D Map':
     heatmap(df)
